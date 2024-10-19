8a0aabcecbdf3d4272bc10b3f95f334cc4809bfe
yaofaqian <1061397953@qq.com>
2024-10-19 15:49:39 +0800

    Update io_process.sh

--- io_process.sh
+++ io_process.sh
@@ -2,8 +2,7 @@
 
 # 定义日志文件
 TOP_LOG="top_log.txt"
-CURRENT_TIME=$(date "+%Y-%m-%d-%H-%M-%S")
-IO_LOG="io_log_$CURRENT_TIME.txt"
+IO_LOG="io_log.txt"
 
 # 每隔 5 秒获取一次 top 信息，持续 60 秒
 top -b -n 12 -d 5 > "$TOP_LOG"
@@ -12,10 +11,8 @@ top -b -n 12 -d 5 > "$TOP_LOG"
 > "$IO_LOG"
 
 # 获取当前时间并格式化
+CURRENT_TIME=$(date "+%Y-%m-%d-%H-%M-%S")
 echo "Log Time: $CURRENT_TIME" >> "$IO_LOG"
-
-# 声明一个数组来存储进程的 I/O 信息
-declare -A io_data
 
 # 将 top_log 中的每一行逐行读取
 while IFS= read -r line; do
@@ -32,27 +29,6 @@ while IFS= read -r line; do
     if [[ -d "/proc/$PID" ]]; then
         # 从 /proc/PID/io 获取 I/O 使用情况
         IO_INFO=$(cat /proc/$PID/io)
-
-        # 获取 read_bytes 和 write_bytes
-        READ_BYTES=$(echo "$IO_INFO" | grep "read_bytes" | awk '{print $2}')
-        WRITE_BYTES=$(echo "$IO_INFO" | grep "write_bytes" | awk '{print $2}')
-
-        # 如果没有读取到值，默认设为 0
-        READ_BYTES=${READ_BYTES:-0}
-        WRITE_BYTES=${WRITE_BYTES:-0}
-
-        # 确保 READ_BYTES 和 WRITE_BYTES 是有效数字
-        if ! [[ "$READ_BYTES" =~ ^[0-9]+$ ]]; then
-            READ_BYTES=0
-        fi
-        if ! [[ "$WRITE_BYTES" =~ ^[0-9]+$ ]]; then
-            WRITE_BYTES=0
-        fi
-
-        TOTAL_IO=$((READ_BYTES + WRITE_BYTES))
-
-        # 存储 I/O 信息到数组
-        io_data["$PID"]="$PROCESS_NAME $READ_BYTES $WRITE_BYTES $TOTAL_IO"
 
         # 输出格式化信息
         echo "ProcessName: $PROCESS_NAME" >> "$IO_LOG"
@@ -75,13 +51,5 @@ while IFS= read -r line; do
         echo "-----------------------------" >> "$IO_LOG"
     fi
 done < "$TOP_LOG"
-
-# 按 read_bytes + write_bytes 对 I/O 信息进行排序
-{
-    echo "ProcessName # PID # 读取的字节数 # 写入的字节数 # 总 I/O 字节数"
-    for key in "${!io_data[@]}"; do
-        echo "${io_data[$key]} # $key"
-    done
-} | sort -k4 -n -r >> "$IO_LOG"
 
 echo "I/O information logged to $IO_LOG."
